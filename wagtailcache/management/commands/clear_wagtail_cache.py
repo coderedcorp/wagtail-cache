d695eec2f8f3d36dbc57826c785cf0d4ff4f240b
Hammy Goonan <hammy@liquorice.com.au>
2019-06-18 12:07:25 +1000

    adding cli

---
+++ wagtailcache/management/commands/clear_wagtail_cache.py
@@ -0,0 +1,11 @@
+"""CLI tool to clear wagtailcache."""
+
+from django.core.management.base import BaseCommand
+from wagtailcache.cache import clear_cache
+
+
+class Command(BaseCommand):
+    help = 'Clears the cache for the entire site.'
+
+    def handle(self, *args, **options):
+        clear_cache()
