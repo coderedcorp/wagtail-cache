c0baf0ed5c2330ac02fdfb8223adb4f2a1298648
Vince Salvino <salvino@coderedcorp.com>
2020-07-28 23:57:54 -0400

    Version 1.0.1 release

--- wagtailcache/__init__.py
+++ wagtailcache/__init__.py
@@ -1,4 +1,4 @@
-release = ["1", "0", "0"]
+release = ["1", "0", "1"]
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])
 
