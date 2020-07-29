bc86131657afe2b77012b3ab46d70eda8f53d0f0
Vince Salvino <salvino@coderedcorp.com>
2020-07-28 23:50:33 -0400

    Simplify and cleanup documentation styles

--- docs/conf.py
+++ docs/conf.py
@@ -40,11 +40,10 @@ release = __shortversion__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -76,36 +75,25 @@ pygments_style = 'default'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'sidebarwidth': '260px',
-    'bodyfont': 'initial',
-    'headfont': 'inherit',
-    'footerbgcolor': 'inherit',
-    'footertextcolor': '#888',
-    'sidebarbgcolor': '#001834',
-    'sidebartextcolor': 'rgba(255,255,255,0.7)',
-    'sidebarlinkcolor': 'rgba(255,255,255,0.7)',
-    'relbarbgcolor': 'inherit',
-    'relbartextcolor': '#888',
-    'relbarlinkcolor': '#888',
-    'bgcolor': '#fff',
-    'textcolor': '#000',
-    'linkcolor': '#0aa',
-    'visitedlinkcolor': '#0aa',
-    'headbgcolor': 'inherit',
-    'headtextcolor': 'inherit',
+    'body_text': '#212529',
+    'sidebar_link': '#212529',
+    'sidebar_width': '270px',
+    'page_width': '1000px',
+    'font_family': '"Palatino", "Palatino Linotype", "Linux Libertine", serif',
+    'head_font_family': '"Palatino", "Palatino Linotype", "Linux Libertine", serif',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -115,85 +103,6 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-html_sidebars = {'**': ['searchbox.html', 'globaltoc.html', 'relations.html', 'versions.html']}
-
-html_context = {
-    'css_files': ['_static/docs.css'],
-}
+html_sidebars = {'**': ['about.html', 'searchbox.html', 'globaltoc.html', 'relations.html']}
 
 html_last_updated_fmt = ''
-
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'wagtail-cachedoc'
-
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'wagtail-cache.tex', 'wagtail-cache Documentation',
-     'CodeRed LLC', 'manual'),
-]
-
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'wagtail-cache', 'wagtail-cache Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'wagtail-cache', 'wagtail-cache Documentation',
-     author, 'wagtail-cache', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,26 @@
 Wagtail Cache Documentation
 ===========================
 
-Wagtail Cache is a simple page cache for Wagtail inspired by the Django cache middleware.
-It provides an extremely fast way of serving pages with no database hits whatsoever.
-It is customizable enough that the developer can determine whether or not to cache
-on each individual request/response, or even on the Wagtail page models directly!
+Wagtail Cache is a simple page cache for Wagtail inspired by the Django cache
+middleware. It provides an extremely fast way of serving pages with no database
+hits whatsoever. It is customizable enough that the developer can determine
+whether or not to cache on each individual request/response, or even on the
+Wagtail page models directly!
 
-Wagtail Cache also provides a panel to show cache stats and clear the cache
-from the wagtail admin under Settings > Cache.
+Wagtail Cache also provides a panel to show cache stats and clear the cache from
+the wagtail admin under **Settings > Cache**.
+
+`GitHub <https://github.com/coderedcorp/wagtail-cache>`_ |
+`Documentation <https://docs.coderedcorp.com/wagtail-cache/>`_ |
+`PyPI <https://pypi.org/project/wagtail-cache/>`_
 
 
 Notes
 -----
 
-This cache feature was originally part of `coderedcms <https://github.com/coderedcorp/coderedcms>`_
-and is in use successfully on live production sites.
+This cache feature was originally part of `coderedcms
+<https://github.com/coderedcorp/coderedcms>`_ and is in use successfully on live
+production sites.
 
 Requires Wagtail >= 2.0
 
