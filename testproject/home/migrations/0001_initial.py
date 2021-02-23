92c4f8baf2382ede28942564e25327e5e6d9a210
Roxanna Coldiron <roxanna@coderedcorp.com>
2021-02-23 11:05:25 -0500

    removed black formatting in migrations, added the toml file to tell black to ignore migrations

--- testproject/home/migrations/0001_initial.py
+++ testproject/home/migrations/0001_initial.py
@@ -8,88 +8,48 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("wagtailcore", "0040_page_draft_title"),
+        ('wagtailcore', '0040_page_draft_title'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name="CacheControlPage",
+            name='CacheControlPage',
             fields=[
-                (
-                    "page_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="wagtailcore.Page",
-                    ),
-                ),
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
-            bases=(wagtailcache.cache.WagtailCacheMixin, "wagtailcore.page"),
+            bases=(wagtailcache.cache.WagtailCacheMixin, 'wagtailcore.page'),
         ),
         migrations.CreateModel(
-            name="CachedPage",
+            name='CachedPage',
             fields=[
-                (
-                    "page_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="wagtailcore.Page",
-                    ),
-                ),
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
-            bases=(wagtailcache.cache.WagtailCacheMixin, "wagtailcore.page"),
+            bases=(wagtailcache.cache.WagtailCacheMixin, 'wagtailcore.page'),
         ),
         migrations.CreateModel(
-            name="CallableCacheControlPage",
+            name='CallableCacheControlPage',
             fields=[
-                (
-                    "page_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="wagtailcore.Page",
-                    ),
-                ),
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
-            bases=(wagtailcache.cache.WagtailCacheMixin, "wagtailcore.page"),
+            bases=(wagtailcache.cache.WagtailCacheMixin, 'wagtailcore.page'),
         ),
         migrations.CreateModel(
-            name="WagtailPage",
+            name='WagtailPage',
             fields=[
-                (
-                    "page_ptr",
-                    models.OneToOneField(
-                        auto_created=True,
-                        on_delete=django.db.models.deletion.CASCADE,
-                        parent_link=True,
-                        primary_key=True,
-                        serialize=False,
-                        to="wagtailcore.Page",
-                    ),
-                ),
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
             ],
             options={
-                "abstract": False,
+                'abstract': False,
             },
-            bases=("wagtailcore.page",),
+            bases=('wagtailcore.page',),
         ),
     ]
