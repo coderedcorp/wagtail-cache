fcf80777cfe65fb4639ae55d83ce3379e8b628d6
Vince Salvino <salvino@coderedcorp.com>
2022-06-07 17:38:05 -0400

    Wagtail 3 support

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "wagtail.images",
     "wagtail.search",
     "wagtail.admin",
-    "wagtail.core",
+    "wagtail",
     "modelcluster",
     "taggit",
     "django.contrib.admin",
