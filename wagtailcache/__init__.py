20e9ad34a655af06fb3e11f44d277e8b505417a0
Vince Salvino <salvino@coderedcorp.com>
2022-08-05 12:55:50 -0400

    Fix Django 5 warnings, check for migrations in pipeline

--- wagtailcache/__init__.py
+++ wagtailcache/__init__.py
@@ -1,5 +1,3 @@
 release = ["2", "0", "0"]
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])
-
-default_app_config = "wagtailcache.apps.WagtailCacheAppConfig"
