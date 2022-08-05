ae936e3ce1baee737f746fb6344de0e60350bdaa
Vince Salvino <salvino@coderedcorp.com>
2022-08-05 16:02:30 -0400

    Bump version to 1.2.1

--- wagtailcache/__init__.py
+++ wagtailcache/__init__.py
@@ -1,4 +1,4 @@
-release = ["1", "2", "0"]
+release = ["1", "2", "1"]
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])
 
