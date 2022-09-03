87c9e3c00284ea2c2cf5ca2010f0881f1de9387c
Grzegorz Krol <gk@cjk.pl>
2022-09-03 10:58:08 +0200

    Allowed compatibility with Wagtail 4.0. Bumped version to 2.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="BSD license",
     include_package_data=True,
     packages=find_packages(),
-    install_requires=["wagtail>=3.0,<4.0"],
+    install_requires=["wagtail>=3.0,<=4.0"],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
