772ccbcfe364ee29591efea38aad44ee96d1d6d9
Grzegorz Krol <gk@cjk.pl>
2022-10-11 06:20:31 +0200

    Jonatron's fix to the Django 4.1.x compatibility issue - thank you, @jonatron (https://github.com/jonatron)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="BSD license",
     include_package_data=True,
     packages=find_packages(),
-    install_requires=["wagtail>=3.0,<5.0", "django>=3.0,<4.1"],
+    install_requires=["wagtail>=3.0,<5.0"],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
